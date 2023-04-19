9b2cc91252bfc720340b99b4d38e42f30a8e29ff
nitish@gaga <nitishgoku01@gmail.com>
2023-04-19 17:41:48 +0530

    creating application card for company

--- client/src/components/company/ApplicationCard.js
+++ client/src/components/company/ApplicationCard.js
@@ -1,40 +1,36 @@
 import React from 'react'
 
 const ApplicationCard = ({ props }) => {
-  const { name, email, phone } = props
+  const { name, email, phone, imgUrl,resumeLink } = props
   console.log(props)
   return (
     <div className="max-w-lg p-4">
-      <article class="rounded-xl border border-gray-700 bg-gray-800 p-4">
-        <div class="flex items-center gap-4">
+      <article className="rounded-xl border border-gray-700 bg-gray-800 p-4">
+        <div className="flex items-center gap-4">
           <img
-            alt="Developer"
-            src="https://images.unsplash.com/photo-1614644147724-2d4785d69962?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80"
-            class="h-16 w-16 rounded-full object-cover"
+            alt="Applicant"
+            src={imgUrl || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAhDmdBbYxs30OcmcYLM4OYdU8z4DeiNKEdD0X3e-U3Q&usqp=CAU&ec=48600112"}
+            className="h-16 w-16 rounded-full object-cover"
           />
 
           <div>
-            <h3 class="text-lg font-medium text-white">Claire Mac</h3>
+            <h3 className="text-lg font-medium text-white">{name}</h3>
 
-            <div class="flow-root">
-              <ul class="-m-1 flex flex-wrap">
-                <li class="p-1 leading-none">
-                  <a href="#" class="text-xs font-medium text-gray-300">
-                    {' '}
-                    Twitter{' '}
+            <div className="flow-root">
+              <ul className="-m-1 flex flex-wrap items-center">
+                <li className="p-1">
+                  <a href={"mailto:"+email} target='_blank' className="text-xs font-medium text-gray-300">
+                    {email} |
                   </a>
                 </li>
-
-                <li class="p-1 leading-none">
-                  <a href="#" class="text-xs font-medium text-gray-300">
-                    {' '}
-                    GitHub{' '}
-                  </a>
+                <li className="p-1">
+                  <p className='text-xs translate-y-[2px] font-medium text-gray-300'>
+                  {"9641253871"} |
+                  </p>
                 </li>
-
-                <li class="p-1 leading-none">
-                  <a href="#" class="text-xs font-medium text-gray-300">
-                    Website
+                <li className="p-1">
+                  <a href={resumeLink||""} target='_blank' className="text-xs font-medium text-gray-300">
+                  Resume Link
                   </a>
                 </li>
               </ul>
@@ -42,15 +38,15 @@ const ApplicationCard = ({ props }) => {
           </div>
         </div>
 
-        <ul class="mt-4 space-y-2">
+        {/* <ul className="mt-4 space-y-2">
           <li>
             <a
               href="#"
-              class="block h-full rounded-lg border border-gray-700 p-4 hover:border-pink-600"
+              className="block h-full rounded-lg border border-gray-700 p-4 hover:border-pink-600"
             >
-              <strong class="font-medium text-white">Project A</strong>
+              <strong className="font-medium text-white">Project A</strong>
 
-              <p class="mt-1 text-xs font-medium text-gray-300">
+              <p className="mt-1 text-xs font-medium text-gray-300">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
                 consequuntur deleniti, unde ab ut in!
               </p>
@@ -60,17 +56,17 @@ const ApplicationCard = ({ props }) => {
           <li>
             <a
               href="#"
-              class="block h-full rounded-lg border border-gray-700 p-4 hover:border-pink-600"
+              className="block h-full rounded-lg border border-gray-700 p-4 hover:border-pink-600"
             >
-              <strong class="font-medium text-white">Project B</strong>
+              <strong className="font-medium text-white">Project B</strong>
 
-              <p class="mt-1 text-xs font-medium text-gray-300">
+              <p className="mt-1 text-xs font-medium text-gray-300">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Sapiente cumque saepe sit.
               </p>
             </a>
           </li>
-        </ul>
+        </ul> */}
       </article>
     </div>
   )
